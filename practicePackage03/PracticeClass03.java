ceb1feea5da661e6dd908d7dae515727a1025f33
Shilpa <shilpa25988@gmail.com>
2023-11-01 22:45:08 +0000

    filemodified

--- practicePackage03/PracticeClass03.java
+++ practicePackage03/PracticeClass03.java
@@ -3,6 +3,7 @@ package practicePackage03;
 public class PracticeClass03 {
     public static void main(String[] args) {
 
-        System.out.println("What is STLC");
+        System.out.println("What is SDLC and STLC?");
+
     }
 }
